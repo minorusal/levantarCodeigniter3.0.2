9206bdd6518e053817defad96c06b367ea700eea
MINORU DANIEL SALAZAR PALAFOX <daniel.salazar@isolution.mx>
2015-10-16 13:06:32 -0500

    add tipografia

---
+++ application/language/es_ES/system_lang.php
@@ -0,0 +1,6 @@
+<?php
+$lang['modal_saludo'] = 'bienvenido';
+$lang['modal_msg']    = 'Usted puede tiene asignado los siguientes perfiles: ';
+$lang['row_perfil']   = 'perfiles';
+$lang['row_pais']     = 'país';
+?>
